a0bc3fc1307b4b3b9e950f6c7ff8e8b365c52c53
David Rose <david.sr.rose@protonmail.com>
2022-01-29 15:07:53 -0700

    formatting updates

--- profit_functions/wynnbet_bonus_only_profit.R
+++ profit_functions/wynnbet_bonus_only_profit.R
@@ -1,64 +1,67 @@
 
-#Runs the 5 wynnbet bonus bets at the given odds, bonus bets and n, returns the avg profit of n runs
+# Runs the 5 wynnbet bonus bets at the given odds, bonus bets and n, returns the avg profit of n runs
 
-#inputs:
-#bonus_odds   the odds used for each bonus bet
-#bonus_bets   the number of the bonus bets which are bet. 5- bonus_bets would be the number converted
-#n            the number of runs
+# inputs:
+# bonus_odds   the odds used for each bonus bet
+# bonus_bets   the number of the bonus bets which are bet. 5- bonus_bets would be the number converted
+# n            the number of runs
 
-#outputs: 
-#avg_profit
+# outputs:
+# avg_profit
 
-#notes on improvements and additions to function
+# notes on improvements and additions to function
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------
-wynnbet_bonus_only_profit <- function(bonus_odds = 400,bonus_bets = 0,n=1){
-  
-  #profit for our single run
+wynnbet_bonus_only_profit <- function(bonus_odds = 400,
+                                      bonus_bets = 0,
+                                      n = 1) {
+
+  # profit for our single run
   profit <- 0
-  #profit total all runs
+  # profit total all runs
   total_profit <- 0
-  #avg profit
-  avg_profit <-0
-  
-  #bonus betting sets to null to start   #theres got to be a better way to do this than those 
+  # avg profit
+  avg_profit <- 0
+
+  # bonus betting sets to null to start
+  # theres got to be a better way to do this than those
   bonus_bets_bet <- c(NULL)
   bonus_bets_converted <- c(NULL)
-  
-  #set of bonus bets bet
-  if(bonus_bets > 0){
+
+  # set of bonus bets bet
+  if (bonus_bets > 0) {
     bonus_bets_bet <- c(1:bonus_bets)
   } else {
     bonus_bets_bet <- NULL
   }
-  
-  #set of bonus bets converted
-  if( (5-bonus_bets)> 0){
-    bonus_bets_converted <- c(1:(5-bonus_bets))
+
+  # set of bonus bets converted
+  if ((5 - bonus_bets) > 0) {
+    bonus_bets_converted <- c(1:(5 - bonus_bets))
   } else {
     bonus_bets_converted <- NULL
   }
-  
-  for (k in 1:n){
-    #free bets bet
-    for (i in bonus_bets_bet){#free bets
-      if(runif(1) < implied_p(bonus_odds)){
-        #win the bonus bet. Note that we only get the payout, no stake back w/free bet play
-        total_profit <- total_profit  + 300*payout_fraction(bonus_odds)
-        #print(paste("bonus win",profit))
+
+  for (k in 1:n) {
+    # free bets bet
+    for (i in bonus_bets_bet) { # free bets
+      if (runif(1) < implied_p(bonus_odds)) {
+        # win the bonus bet. Note that we only get the payout, no stake back w/free bet play
+        total_profit <- total_profit + 300 * payout_fraction(bonus_odds)
+        # print(paste("bonus win",profit))
       } else {
-        #print(paste("bonus loss",profit))
+        # print(paste("bonus loss",profit))
       }
     }
-    #free bets converted
-    for (i in bonus_bets_converted){
-      total_profit <- total_profit + 300*.7
-      #print(paste("bonus convert",profit))
+    # free bets converted
+    for (i in bonus_bets_converted) {
+      total_profit <- total_profit + 300 * .7
+      # print(paste("bonus convert",profit))
     }
   }
-  
-  #calculate avg profit of all runs
-  avg_profit <- total_profit/n
-  
+
+  # calculate avg profit of all runs
+  avg_profit <- total_profit / n
+
   return(avg_profit)
 }
