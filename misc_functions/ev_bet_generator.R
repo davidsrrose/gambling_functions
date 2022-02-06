8ef0c2834a3934ada7ea4eb414d86e8b32b62bc6
David Rose <david.sr.rose@protonmail.com>
2022-02-06 09:01:12 -0700

    outsourced dataframe filtering to new function

--- misc_functions/ev_bet_generator.R
+++ misc_functions/ev_bet_generator.R
@@ -1,43 +1,56 @@
-# returns odds and fair win probabilty for a randomly selected bet from ev_bets
+# returns odds and fair win probabilty for a randomly selected bet from input dataframe
 
 # inputs
-# min_ev - minimum ev of bets we want to place
+# min_ev - min ev of bets we want in our returned dataframe
+# max_ev - max ev of bets we want in our returned dataframe
+# min odds - max odd of bets we want in our returned dataframe
+# max odds -min odd of bets we want in our returned dataframe
 # type - type of bet we want to place "standard" or "live" or "both"
 
 # outputs
-# vector of odds and fair win probabilty for ev bet we want to make
+# bet parameters matrix - [ev bet odd, ev bet fair win probability]
 
 # notes on updates and improvements
 
 #-------------------------------------------------------------------------------
-ev_bet_generator <- function(min_ev = .05, bet_type = "standard") {
+ev_bet_generator <- function(min_ev = -Inf,
+                             max_ev = Inf,
+                             min_odd = -Inf,
+                             max_odd = Inf,
+                             bet_type = "both",
+                             bet_df_to_filter = ev_bet_df) {
 
   # initialize variables
-  ev_bet_parameters <- c(0, 0)
-  bet_row <- 0
-  ev_bets_sample_df <- data.frame(NULL)
+  # random row of df for bet
+  random_bet_row <- 0
 
-  # select ev_bets_sample_df for min ev
-  ev_bets_sample_df <- ev_bets[ev_bets$ev >= min_ev, ]
+  # bet parameters matrix
+  ev_bet_parameters <- matrix(0, nrow = 1, ncol = 2)
+  colnames(ev_bet_parameters) <- c("odds", "fair_win_p")
 
-  # select for type of not "both"
-  if (bet_type != "both") {
-    ev_bets_sample_df <- ev_bets_sample_df[ev_bets_sample_df$bet_type == bet_type, ]
-  }
+  # sample dataframe to pick from
+  ev_bet_sample_df <- data.frame(NULL)
+  ev_bet_sample_df <- ev_bet_df_filter(
+    min_ev = min_ev,
+    max_ev = max_ev,
+    min_odd = min_odd,
+    max_odd = max_odd,
+    bet_type = bet_type,
+    bet_df_to_filter = bet_df_to_filter
+  )
 
-  # randomly select bet from from ev_bets dataframe, based on parameters
-  bet_row <- sample(1:nrow(ev_bets), 1)
+  # randomly select bet row from from ev_bets dataframe, based on parameters
+  random_bet_row <- sample(1:nrow(ev_bet_sample_df), 1)
 
   # set odds and fair win probability
   # odds for bet
-  ev_bet_parameters[1] <- ev_bets$ev_odd[bet_row] # nolint
-
+  ev_bet_parameters[1] <- ev_bet_sample_df$ev_odd[random_bet_row] # nolint
 
   # fair win probability for bet
   ev_bet_parameters[2] <-
     no_vig_p(
-      ev_bets$oddsj_ev_odd[bet_row],
-      ev_bets$oddsj_ev_odd_opp[bet_row]
+      ev_bet_sample_df$oddsj_ev_odd[random_bet_row],
+      ev_bet_sample_df$oddsj_ev_odd_opp[random_bet_row]
     )
 
   return(ev_bet_parameters)
