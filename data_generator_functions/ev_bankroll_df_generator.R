5d1841f298dd7c589ac5d3a63ed3c5a4c6f7fa55
David Rose <david.sr.rose@protonmail.com>
2022-02-06 18:09:18 -0700

    updated from generating global to local dataframe

--- data_generator_functions/ev_bankroll_df_generator.R
+++ data_generator_functions/ev_bankroll_df_generator.R
@@ -13,12 +13,12 @@ ev_bankroll_df_generator <- function(bets = 1000,
 
   # initialize variables
   # bankroll dataframe
-  ev_bankroll_df <<- data.frame(matrix(ncol = 6, nrow = bets))
+  ev_bankroll_df <- data.frame(matrix(ncol = 6, nrow = bets))
   # ev bet parameters
   ev_bet_parameters <- c(0, ncol = 2)
 
   # bankroll dataframe
-  colnames(ev_bankroll_df) <<- c(
+  colnames(ev_bankroll_df) <- c(
     "bankroll",
     "stake",
     "odds",
@@ -28,7 +28,7 @@ ev_bankroll_df_generator <- function(bets = 1000,
   )
 
   # set bankroll at first bet
-  ev_bankroll_df$bankroll[1] <<- bankroll_start
+  ev_bankroll_df$bankroll[1] <- bankroll_start
 
   # loop through number of bets specified
   for (bet_row in 1:bets) {
@@ -36,13 +36,13 @@ ev_bankroll_df_generator <- function(bets = 1000,
     ev_bet_parameters <- ev_bet_generator()
 
     # bet odds
-    ev_bankroll_df$odds[bet_row] <<- ev_bet_parameters[1]
+    ev_bankroll_df$odds[bet_row] <- ev_bet_parameters[1]
 
     # bet fair win probability
-    ev_bankroll_df$fair_win_p[bet_row] <<- ev_bet_parameters[2]
+    ev_bankroll_df$fair_win_p[bet_row] <- ev_bet_parameters[2]
 
     # bet stake
-    ev_bankroll_df$stake[bet_row] <<-
+    ev_bankroll_df$stake[bet_row] <-
       ev_bankroll_df$bankroll[bet_row] *
         kelly_fraction(
           odds = ev_bankroll_df$odds[bet_row],
@@ -65,22 +65,23 @@ ev_bankroll_df_generator <- function(bets = 1000,
     # make the bet - and fill bet profit, updated bankroll value fields
     if (runif(1) < ev_bankroll_df$fair_win_p[bet_row]) {
       # win bet, set profit
-      ev_bankroll_df$bet_profit[bet_row] <<-
+      ev_bankroll_df$bet_profit[bet_row] <-
         ev_bankroll_df$stake[bet_row] *
           payout_fraction(ev_bankroll_df$odds[bet_row])
     } else {
       # lose bet, set profit
-      ev_bankroll_df$bet_profit[bet_row] <<- -ev_bankroll_df$stake[bet_row]
+      ev_bankroll_df$bet_profit[bet_row] <- -ev_bankroll_df$stake[bet_row]
     }
 
     # set updated bankroll value
-    ev_bankroll_df$post_bet_bankroll[bet_row] <<-
+    ev_bankroll_df$post_bet_bankroll[bet_row] <-
       ev_bankroll_df$bankroll[bet_row] +
       ev_bankroll_df$bet_profit[bet_row]
 
     # set next row bankroll start - except for last row (bets)
     if (bet_row < bets) {
-      ev_bankroll_df$bankroll[bet_row + 1] <<- ev_bankroll_df$post_bet_bankroll[bet_row]
+      ev_bankroll_df$bankroll[bet_row + 1] <-
+        ev_bankroll_df$post_bet_bankroll[bet_row]
     }
   }
 
