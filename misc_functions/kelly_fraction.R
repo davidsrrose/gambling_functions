adb8588aa7fe0a6b1c662ffba6027e46a04ddaff
David Rose <david.sr.rose@protonmail.com>
2022-02-06 18:09:26 -0700

    formatting

--- misc_functions/kelly_fraction.R
+++ misc_functions/kelly_fraction.R
@@ -14,6 +14,7 @@ kelly_fraction <- function(odds = 400,
                            kelly_multiplier = 1) {
   # set kelly fraction
   kelly_fraction <- kelly_multiplier *
-    (fair_win_p - (1 - fair_win_p) / payout_fraction(odds))
+    (fair_win_p - (1 - fair_win_p) / 
+       payout_fraction(odds))
   return(kelly_fraction)
 }
